7b605823305eebb61152aff3c4e383378c864cb3
Gaurav Garg <44519458+garggaurav730@users.noreply.github.com>
2021-03-28 18:02:25 +0530

    Add files via upload

---
+++ GauravGarg_MusicPlayer/script.js
@@ -0,0 +1,11 @@
+// CODE FOR HAMBURGER ICON STARTS
+
+function onClickMenu(){
+	document.getElementById("menu").classList.toggle("change");
+	document.getElementById("nav").classList.toggle("change");
+
+	document.getElementById("menu-bg").classList.toggle("change-bg");
+}
+
+// CODE FOR HAMBURGER ICON ENDSS
+
